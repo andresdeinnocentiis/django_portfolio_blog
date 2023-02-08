51f763fdc9fbf3fb13ffed23fd67362d1d1667fd
andresdeinnocentiis <andresdeinnocentiis@gmail.com>
2023-02-08 17:23:20 -0300

    fix

--- portfolio_blog/templates/pages/Post/increment.py
+++
@@ -1,8 +0,0 @@
-from django import template
-register = template.Library()
-
-# I'm using this in order to increment a counter inside a Django template to change color of each comment div
-
-@register.filter
-def increment(value):
-    return value + 1
