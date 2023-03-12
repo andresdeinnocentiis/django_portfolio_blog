7a66323f662178375112c92d5de7c2612c8046ab
andresdeinnocentiis <andresdeinnocentiis@gmail.com>
2023-03-12 04:41:58 -0300

    Fixes

--- frontend/vue_app/src/components/About/ExperienceSection.vue
+++ frontend/vue_app/src/components/About/ExperienceSection.vue
@@ -24,6 +24,43 @@
                 </div>
             </div>
         </div>
+        <div class="work-experience">
+            <h3 class="exp-title">Academic Background</h3>
+            <div class="work-exp-container">
+                <div class="work-exp-row">
+                    <div class="exp-years">2023 - to date</div>
+                    <div class="exp-job"><span>Blockchain Developer</span> at Udemy</div>
+                </div>
+                <div class="work-exp-row">
+                    <div class="exp-years">2022 - 2023</div>
+                    <div class="exp-job"><span>Django Developer</span> at National Technological University</div>
+                </div>
+                <div class="work-exp-row">
+                    <div class="exp-years">2021 - 2022</div>
+                    <div class="exp-job"><span>Django Developer & Python specialization</span> at Inove Academy</div>
+                </div>
+                <div class="work-exp-row">
+                    <div class="exp-years">2020 - 2021</div>
+                    <div class="exp-job"><span>Frontend Developer Bootcamp</span> at Coderhouse</div>
+                </div>
+                <div class="work-exp-row">
+                    <div class="exp-years">2020 - 2021</div>
+                    <div class="exp-job"><span>Python Programmer</span> at IT Master Academy</div>
+                </div>
+                <div class="work-exp-row">
+                    <div class="exp-years">2017 & 2019</div>
+                    <div class="exp-job"><span>Russian Language</span> at Pskov State University</div>
+                </div>
+                <div class="work-exp-row">
+                    <div class="exp-years">2014 - 2018</div>
+                    <div class="exp-job"><span>French Language</span> at Alliance Française</div>
+                </div>
+                <div class="work-exp-row">
+                    <div class="exp-years">2012 - 2018</div>
+                    <div class="exp-job"><span>Lawyer</span> at University of Buenos Aires</div>
+                </div>
+            </div>
+        </div>
 
     </div>
 </template>
